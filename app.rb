1e02e8a4018e0bd72d10d4800bf22e463d5f1ed6
Rob Cobb <rwcobbjr@gmail.com>
2018-04-02 11:59:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb 'pirates/new'.to_sym
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+      @ships = params[:pirate][:ships].map do |ship|
+        Ship.new(ship[:name], ship[:type], ship[:booty])
+      end
+      erb 'pirates/show'.to_sym
+    end
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+  end
+
+  def self.clear
+    @@all = []
+  end
+  clear
+
+  def self.all
+    @@all
+  end
 end
